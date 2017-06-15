fce5fd49c4b1ae28b7ca385ca5e70fab7e975635
bradymat <bradymat@gmail.com>
2017-06-15 15:31:24 +1200

    Updates

--- initialState.js
+++ initialState.js
@@ -2,7 +2,6 @@ module.exports = () => {
   return {
     name: "iEi Otago",
     menu: [
-      { page:"Services", route:"/Services" },
       { page:"About", route:"/About" },
       { page:"FAQ", route:"/FAQ" },
       { page:"Contact", route:"/Contact" }
@@ -11,6 +10,7 @@ module.exports = () => {
     services: [
       {
         service:"Property Inspection",
+        image: "https://thumb7.shutterstock.com/display_pic_with_logo/53073/53073,1196472714,1/stock-photo-exterior-of-a-typical-small-town-house-in-rural-new-zealand-7426006.jpg",
         content: [
           {heading: "", content:"There are many reasons why you could need an electrical test or inspection on your property or electrical items. It could be for insurance purposes, landlord's inspection, change of use, ownership or even just for your own peace of mind."},
           {heading: "Did You Know?", content:"All electrical systems deteriorate with age and/or use so it is important that they are maintained regularly to ensure that the safety of people around the system are not at risk."},
@@ -28,6 +28,7 @@ module.exports = () => {
       },
       {
         service:"Electrical Warrant of Fitness",
+        image: "https://thumb7.shutterstock.com/display_pic_with_logo/648904/500827201/stock-photo-caravan-trailer-on-a-green-lawn-on-a-sunny-spring-day-500827201.jpg",
         content: [
           {heading: "", content:"iEi Otago can take the hassle out of your next Electrical Warrant of Fitness (EWOF) for your caravan, motorhome, mobile food van or boat. Our Inspectors can come to where the vehicle is located for your convenience."},
           {heading: "Did You Know?", content:"It is a requirement under the Electricity Act 1992 that any connectable installation be certified before being connected to a power supply. For example, holiday park operators will require your caravan or motorhome to have a current EWOF before allowing you to connect on a powered site. Another example is, a local authority will require a current EWOF for a mobile food van before connecting into a public power point."},
@@ -43,6 +44,7 @@ module.exports = () => {
       },
       {
         service:"Explosive Area Inspection",
+        image: "https://thumb7.shutterstock.com/display_pic_with_logo/3212969/579910117/stock-photo-gas-station-fuel-pump-579910117.jpg",
         content: [
           {heading: "", content:"iEi Otago carries out explosive area inspections to AS/NZS:60079 standards for commercial businesses who have explosive environments  associated with their work activities."},
           {heading: "Did You Know?", content:"Many business activities involve the use of airborne particles and inflammatory materials. These may ignite if an electrical fault occurred within the work area. These areas listed here require explosive area inspection: Fuel stations, Paint spraying and tinting booths &amp; mixing rooms, Dust environments, LPG stations &amp; storage facilities, Dangerous Goods stores, Printing areas, Woodworking dust areas, Bakeries, Silos, Industrial applications, DG Store certification/ verification"},
@@ -58,6 +60,7 @@ module.exports = () => {
       },
       {
         service:"Certificate of Verification",
+        image: "https://thumb9.shutterstock.com/display_pic_with_logo/4574812/581376208/stock-photo-high-voltage-post-high-voltage-tower-sky-background-581376208.jpg",
         content: [
           {heading: "Power Disconnected for 6 months or more?", content:"Where power is not used over long periods of time, people often have their electricity supply disconnected. It is important to note that if the power has been disconnected for longer than six months, your installation must (by law) be inspected before reconnection can occur. This applies to all installations including temporary supplies for builders, pumps, cowsheds, houses and cribs, regardless of age."},
           {heading: "Not Sure How Long your Power has been Disconnected?", content:"Call your power provider with the address. They will be able to tell you exactly how long the electricity has been turned off and advise whether an inspection is required."}
@@ -71,6 +74,7 @@ module.exports = () => {
       },
       {
         service:"New Building Connection",
+        image: "https://thumb7.shutterstock.com/display_pic_with_logo/537709/209541841/stock-photo-crane-and-building-construction-site-against-blue-sky-209541841.jpg",
         content: [
           {heading: "", content:"If you are building a new house or require a new power supply, you must seek approval before connecting to the local power network. Network approval is intended to ensure the following: The connection is made to the correct supply point, There is legal access to the network, Network capacity is sufficient, Fusing is available"}
         ],
@@ -84,6 +88,7 @@ module.exports = () => {
       },
       {
         service:"Permanent Disconnection",
+        image: "https://thumb1.shutterstock.com/display_pic_with_logo/789982/162319469/stock-photo-hydraulic-crusher-excavator-machine-at-site-demolition-162319469.jpg",
         content: [
           {heading: "Demolishing or Removing a Building?", content:"For the safety of everyone, it is important to have the electricity service disconnected before you begin demolition or removal of a building."}
         ],
@@ -96,6 +101,7 @@ module.exports = () => {
       },
       {
         service:"Electrical Upgrade",
+        image: "https://thumb7.shutterstock.com/display_pic_with_logo/288118/127057241/stock-photo-senior-man-installing-a-bulb-in-a-freshly-renovated-appartment-127057241.jpg",
         content: [
           {heading: "", content:"As a property ages or is altered, it often becomes essential to replace or change certain elements of your electrical installation. New Zealand wiring regulations states that certain electrical work is high-risk when it is being carried out, and requires independent 3 rd party inspection."}
         ],
